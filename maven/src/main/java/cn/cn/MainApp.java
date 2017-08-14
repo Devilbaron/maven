54443c8e0060f2084b033799b8b42a20963581e7
bingzhang_q@live.cn <dabing2312@163.com>
2017-08-15 00:06:47 +0800

    测试

---
+++ maven/src/main/java/cn/cn/MainApp.java
@@ -0,0 +1,29 @@
+package cn.cn;
+
+import cn.cn.dao.UsersDAO;
+import cn.cn.dbentity.Users;
+import cn.cn.util.SQLTime;
+
+/**
+ * A Camel Application
+ */
+public class MainApp {
+    public static void main(String args[])
+    {
+        try {
+
+            Users s = new Users(5,"小智","451236",23,"男","北京市大兴区",new SQLTime().SQLDateTime());
+            UsersDAO udao = new UsersDAO();
+            if (udao.addStudent(s)){
+                System.out.println("添加成功");
+            }else
+            {
+                System.out.println("添加失败");
+            }
+        }catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
+
